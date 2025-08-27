6ee3e68a9ff0b2bef62a18ebae6ea9883f6d41ee
yueyinqiu <yueyinqiu@outlook.com>
2025-08-27 22:02:48 +0800

    wk

--- src/variable_declaration_checker/__init__.py
+++ src/variable_declaration_checker/__init__.py
@@ -1,4 +1,5 @@
 import astroid as _astroid
+import astroid.nodes as _nodes
 from pylint.checkers import BaseChecker as _BaseChecker
 from pylint.lint import PyLinter as _PyLinter
 
@@ -6,21 +7,21 @@ from pylint.lint import PyLinter as _PyLinter
 class VariableDeclarationChecker(_BaseChecker):
     name = "variable-declaration-checker"
     msgs = {
-        "W5901": ("Declare the variable with type annotation.",
+        "W5901": ("Declare the variable \"%s\" with type annotation.",
                   "un-declared-variable",
                   "It's the first time the variable appears, so here should be a type annotation."),
-        "W5902": ("Remove the type annotation, or rename the variable.",
+        "W5902": ("Remove the type annotation for \"%s\", or use a new variable name.",
                   "re-declared-variable",
                   "The variable has been used before, so here shouldn't be a type annotation."),
     }
 
-    def _check(self, node: _astroid.nodes.LocalsDictNodeNG):
+    def _check(self, node: _nodes.LocalsDictNodeNG):
         # locals 中会包含 nonlocal 的变量：
         # https://github.com/pylint-dev/astroid/issues/2616
         non_locals = {
             name
-            for node in node.nodes_of_class(_astroid.nodes.Nonlocal,
-                                            _astroid.nodes.FunctionDef)
+            for node in node.nodes_of_class(_nodes.Nonlocal,
+                                            _nodes.FunctionDef)
             for name in node.names
         }
         for name, occurrences in node.locals.items():
@@ -31,18 +32,19 @@ class VariableDeclarationChecker(_BaseChecker):
             if name not in non_locals:
                 occurrence = next(occurrence_iterator)
                 if (
-                        isinstance(occurrence, _astroid.nodes.AssignName) and
-                        (not isinstance(occurrence.parent, _astroid.nodes.AnnAssign)) and
-                        (not isinstance(occurrence.parent, _astroid.nodes.Arguments)) and
-                        (not isinstance(occurrence.parent, _astroid.nodes.TypeAlias))
+                    isinstance(occurrence, _nodes.AssignName) and
+                    (not isinstance(occurrence.parent, _nodes.AnnAssign)) and
+                    (not isinstance(occurrence.parent, _nodes.Arguments)) and
+                    (not isinstance(occurrence.parent, _nodes.TypeAlias))
                 ):
-                    self.add_message("un-declared-variable", node=occurrence)
+                    self.add_message("un-declared-variable", node=occurrence, args=name)
 
             for occurrence in occurrence_iterator:
                 if (
-                        isinstance(occurrence.parent, _astroid.AnnAssign)
+                    isinstance(occurrence, _astroid.NodeNG) and
+                    isinstance(occurrence.parent, _astroid.AnnAssign)
                 ):
-                    self.add_message("re-declared-variable", node=occurrence)
+                    self.add_message("re-declared-variable", node=occurrence, args=name)
 
     def visit_functiondef(self, node: _astroid.FunctionDef):
         self._check(node)
